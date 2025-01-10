b4a034f2d29a2074a7c233a1a22905138382eb13
valcyle <yamada.childlen.schedule@gmail.com>
2025-01-10 23:59:32 +0900

    [Change] Testing for forge mod

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const mineflayer = require('mineflayer');
 const { lookNearestEntity } = require('./skills/behavior/lookNearestEntity');
 const { pathfinder } = require('mineflayer-pathfinder');
 const { pathfindToEntity } = require('./skills/behavior/pathfindToEntity');
+// const mineflayerViewer = require('prismarine-viewer').mineflayer;
+var autoVersionForge = require('minecraft-protocol-forge').autoVersionForge;
 
 const bot = mineflayer.createBot({
-    host: 'localhost', // optional
+    host: 'tanakalabo.net', // optional
     port: 25565,       // optional
     username: 'Axe', // email and password are required only for online-mode
-    version: '1.21.4',
+    // version: '1.21.4',
+    version: false, //forge test
     offline: true,
 })
+
+autoVersionForge(bot._client);
 
 bot.loadPlugin(pathfinder);
 
@@ -19,8 +24,9 @@ bot.on('login', () => {
 })
 
 bot.once('spawn', () => {
+    // mineflayerViewer(bot, { port: 3000 }) // Start the viewing server on port 3000
     setTimeout(() => {
-        const target = bot.players['Valcyle'];
+        const target = bot.players['KENkyuuin'];
         console.log(bot.players);
         pathfindToEntity(bot, target);
     }, 1000);
